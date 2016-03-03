02912d7db6a8974371e57f9566c16a2b755bccfc
teamThevibe <team.thevibe@gmail.com>
2016-03-03 14:16:51 +0200

    init

---
+++ create.cmd
@@ -0,0 +1,5 @@
+cd C:\GitHub\repository.thevibe.jarvis\zips
+C:\GitHub\repository.thevibe.jarvis\zips\repo_update.py
+
+move C:\GitHub\repository.thevibe.jarvis\zips\addons.xml  C:\GitHub\repository.thevibe.jarvis\addons.xml
+move C:\GitHub\repository.thevibe.jarvis\zips\addons.xml.md5 C:\GitHub\repository.thevibe.jarvis\addons.xml.md5
